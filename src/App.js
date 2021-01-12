8d94cb79a813a689b8b4004f80425ffd8955b7b3
david espinal <despinal0425@gmail.com>
2021-01-11 19:47:45 -0500

    finished the code along

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from "./components/HomePage";
+import { useState } from "react";
+
+
+//state is like a repository of data
+//hooks: state (you put a default value but it is immutable, unless you
+//use the useState), useEffect, and useRef
 
 function App() {
+  //useState is a hook
+  //itemCurrentlyOnSale (current value)
+  /* setItemCurrentlyOnSale (make current value mutable) or make
+  itemCurrentlyOnSale dynamic */
+  const [itemCurrentlyOnSale, setItemCurrentlyOnSale] = useState('A Hammer'); 
+  const [editable, setEditable] = useState(true);
+
+  //event is when we click the button
+  const toggleEditSaleItem = (event) => setEditable(!editable);
+
+  const handleItemCurrentlyOnSaleChange = (event) => {
+    const itemCurrentlyOnSale = event.target.value;
+    setItemCurrentlyOnSale(itemCurrentlyOnSale);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage saleItem={itemCurrentlyOnSale}
+        editable={editable} toggleEditSaleItem={toggleEditSaleItem}
+        handleItemCurrentlyOnSaleChange={handleItemCurrentlyOnSaleChange}/>
     </div>
   );
 }
